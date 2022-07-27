5d8bbc1855f8621e0fcc993c3c509562cea9c3f2
Paulo Roberto da Silva <paulorsilva92@outlook.com>
2022-07-26 21:22:50 -0300

    First implementation of Camera Preview

--- app/src/main/java/com/example/simplecamera/ui/CameraFragment.kt
+++ app/src/main/java/com/example/simplecamera/ui/CameraFragment.kt
@@ -1,7 +1,77 @@
 package com.example.simplecamera.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.simplecamera.R
+import com.google.common.util.concurrent.ListenableFuture
 
 
-class CameraFragment : Fragment(R.layout.fragment_camera)
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+    private lateinit var cameraPreview: PreviewView
+    private lateinit var takePictureButton: AppCompatImageView
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cameraPreview = view.findViewById(R.id.camera_preview)
+        takePictureButton = view.findViewById(R.id.take_picture_button)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+
+        if (permissionsGranted()) {
+            configCameraPreview()
+        } else {
+            requestCameraPermission()
+        }
+
+
+    }
+
+    private fun permissionsGranted() =
+        requireActivity().checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestCameraPermission() {
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                configCameraPreview()
+            } else {
+                Toast.makeText(requireContext(), "Didn't work =/", Toast.LENGTH_SHORT).show()
+            }
+        }.launch(Manifest.permission.CAMERA)
+    }
+
+
+    private fun configCameraPreview() {
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder().build()
+        val cameraSelector = CameraSelector.Builder().build()
+
+        preview.setSurfaceProvider(cameraPreview.surfaceProvider)
+
+        var camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+    }
+
+
+}
